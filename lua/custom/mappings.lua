703f6cd5c6c174278fcbdae852f5c2964f265718
jihaiming@megvii.com <jihaiming@megvii.com>
2022-07-18 21:50:05 +0800

    add symbols list use f2

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,10 +1,11 @@
 local M = {}
 
 M.telescope = {
-   n = {
-      ["<leader>re"] = { "<cmd> Telescope resume <CR>", "   resume show" },
-      ["<leader>fs"] = { "<cmd> Telescope grep_string <CR>", "   grep_string" },
-  },
+    n = {
+        ["<leader>re"] = { "<cmd> Telescope resume <CR>", "   resume show" },
+        ["<leader>fs"] = { "<cmd> Telescope grep_string <CR>", "   grep_string" },
+        ["<F2>"] = { "<cmd> Telescope lsp_document_symbols <CR>", "   list document symbols" },
+    },
 }
 
 return M
