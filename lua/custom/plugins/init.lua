d38569a66ef9b34ced2c5588700df55756ef6dac
jihaiming@megvii.com <jihaiming@megvii.com>
2022-07-21 10:26:44 +0800

    add go.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,16 +1,18 @@
 return {
-   ["jose-elias-alvarez/null-ls.nvim"] = {
-     after = "nvim-lspconfig",
-   },
-   ["dstein64/vim-startuptime"] = { cmd = "StartupTime" },
-   -- ["morhetz/gruvbox"] = {},
-   ["sainnhe/gruvbox-material"] = {},
-   ["goolord/alpha-nvim"] = {
-     disable = false,
-   },
-   ['lewis6991/gitsigns.nvim'] = {
-     config = function()
-       require('custom.plugins.gitsigns').setup()
-     end
-   },
+    ["jose-elias-alvarez/null-ls.nvim"] = {
+        after = "nvim-lspconfig",
+    },
+    ["dstein64/vim-startuptime"] = { cmd = "StartupTime" },
+    -- ["morhetz/gruvbox"] = {},
+    ["sainnhe/gruvbox-material"] = {},
+    ["goolord/alpha-nvim"] = {
+        disable = false,
+    },
+    ['lewis6991/gitsigns.nvim'] = {
+        config = function()
+            require('custom.plugins.gitsigns').setup()
+        end
+    },
+    ["ray-x/go.nvim"] = {},
+    ["ray-x/guihua.lua"] = {},
 }
