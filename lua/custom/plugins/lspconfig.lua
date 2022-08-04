d2b979f34e0b0cfc153c97a7b7e20c6d488373cd
jihaiming@megvii.com <jihaiming@megvii.com>
2022-08-04 18:30:04 +0800

    update lspconfig

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -3,20 +3,15 @@ local M = {}
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-	-- lspservers with default config
+-- lspservers with default config
 local lspconfig = require "lspconfig"
 local servers = { "gopls", "clangd" }
- 
+
 for _, lsp in ipairs(servers) do
-  local opt = {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
-  if lsp == "gopls" then
-    opt["cmd"] = { "gopls", "--remote=auto" }
-  elseif lsp == "clangd" then
-	opt["cmd"] = { "clangd" }
-  end
-  lspconfig[lsp].setup {opt}
+   lspconfig[lsp].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+   }
 end
 
+return M
