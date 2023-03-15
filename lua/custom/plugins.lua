0b93de587dbd8d43eb343ca1d554ec4f5b6f8885
hankji <jihaimingnet@gmail.com>
2023-03-16 01:49:50 +0800

    for myself update

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,109 @@
+local overrides = require("custom.configs.overrides")
+
+---@type NvPluginSpec[]
+local plugins = {
+
+  -- Override plugin definition options
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+
+  -- overrde plugin configs
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason,
+  },
+
+  -- Install a plugin
+  {
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime",
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    init = require("core.utils").lazy_load "nvim-treesitter-context",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require "custom.configs.treesitter-context"
+        end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    init = require("core.utils").lazy_load "nvim-treesitter-textobjects",
+        -- after = "nvim-treesitter/nvim-treesitter",
+     dependencies = "nvim-treesitter/nvim-treesitter",
+     config = function()
+            require "custom.configs.treesitter-textobjects"
+        end,
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = require("custom.configs.gitsigns").gitsigns,
+  },
+  {
+    "ggandor/leap.nvim",
+    init = require("core.utils").lazy_load "leap.nvim",
+        config = function()
+            require("leap").add_default_mappings()
+            vim.keymap.del({ "x", "o" }, "x")
+            vim.keymap.del({ "x", "o" }, "X")
+        end,
+  },
+  {
+    "anuvyklack/pretty-fold.nvim",
+    init = require("core.utils").lazy_load "pretty-fold.nvim",
+        config = function()
+            require("pretty-fold").setup({
+                remove_fold_markers = false,
+            })
+        end,
+  },
+  {
+    "ThePrimeagen/refactoring.nvim",
+    init = require("core.utils").lazy_load "refactoring.nvim",
+    dependencies = {
+            { "nvim-lua/plenary.nvim" },
+            { "nvim-treesitter/nvim-treesitter" },
+        },
+  },
+
+  -- To make a plugin not be loaded
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   enabled = false
+  -- },
+
+  -- Uncomment if you want to re-enable which-key
+  {
+    "folke/which-key.nvim",
+    enabled = true,
+  },
+}
+
+return plugins
