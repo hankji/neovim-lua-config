a9880d810ecefbe52d5156e658175c4d39a9eb5e
jihaiming@megvii.com <jihaiming@megvii.com>
2023-03-23 19:11:46 +0800

    update chadrc

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -4,9 +4,14 @@ local M = {}
 -- Path to overriding theme and highlights files
 
 M.ui = {
-  theme = "gruvchad",
-  theme_toggle = { "gruvchad", "one_light" },
-  nvdash = {load_on_startup = true},
+  theme = "everforest",
+  theme_toggle = { "everforest", "one_light" },
+  transparency = true,
+  nvdash = { load_on_startup = true },
+  statusline = {
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+    lsprogress_len = 15,
+  },
 }
 
 M.plugins = "custom.plugins"
